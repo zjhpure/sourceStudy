31a98a2c43867903d4fce53b1ce83d3d5516b9f7
pure <714331582@qq.com>
2021-10-17 22:27:04 +0800

    HashMap线程不安全添加具体说明

--- src/main/java/org/pure/sourceStudy/hashMap/Main.java
+++ src/main/java/org/pure/sourceStudy/hashMap/Main.java
@@ -16,6 +16,11 @@ public class Main {
     // 总结：HashMap是线程不安全的，主要体现如下：
     // 在jdk1.7中，在多线程环境下，扩容时会造成环形链或数据丢失。
     // 在jdk1.8中，在多线程环境下，会发生数据覆盖的情况。
+
+    // JDK1.7 中，由于多线程对HashMap进行扩容，调用了HashMap#transfer()。
+    // 具体原因：某个线程执行过程中，被挂起，其他线程已经完成数据迁移，等CPU资源释放后被挂起的线程重新执行之前的逻辑，数据已经被改变，造成死循环、数据丢失。
+    // JDK1.8 中，由于多线程对HashMap进行put操作，调用了HashMap#putVal()。
+    // 具体原因：假设两个线程A、B都在进行put操作，并且hash函数计算出的插入下标是相同的，当线程A执行完第六行代码后由于时间片耗尽导致被挂起，而线程B得到时间片后在该下标处插入了元素，完成了正常的插入，然后线程A获得时间片，由于之前已经进行了hash碰撞的判断，所有此时不会再进行判断，而是直接进行插入，这就导致了线程B插入的数据被线程A覆盖了，从而线程不安全。
     public static void main(String[] args) {
         HashMapThread thread0 = new HashMapThread();
         HashMapThread thread1 = new HashMapThread();
